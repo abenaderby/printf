f51cb2667e5853f28fd00bdc20ca6b23961b05a8
abenaderby <boatemaaharobed@gmail.com>
2023-05-29 12:26:44 -0700

    p

--- write_function.c
+++ write_function.c
@@ -13,30 +13,30 @@
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
-
-	int b = 0;
-	char k = ' ';
+{
+	int i = 0;
+	char pa = ' ';
 
 	UNUSED(precision);
 	UNUSED(size);
 
 	if (flags & F_ZERO)
-		k = '0';
+		pa = '0';
 
-	buffer[b++] = c;
-	buffer[b] = '\0';
+	buffer[i++] = c;
+	buffer[i] = '\0';
 
 	if (width > 1)
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
-		for (b = 0; b < width - 1; b++)
-			buffer[BUFF_SIZE - b - 2] = k;
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = pa;
 
 		if (flags & F_MINUS)
 			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - b - 1], width - 1));
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
-			return (write(1, &buffer[BUFF_SIZE - b - 1], width - 1) +
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 					write(1, &buffer[0], 1));
 	}
 
@@ -56,15 +56,15 @@ int handle_write_char(char c, char buffer[],
  * Return: Number of chars printed.
  */
 int write_number(int is_negative, int ind, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int len = BUFF_SIZE - ind - 1;
-	char k = ' ', extra_ch = 0;
+	char pa = ' ', extra_ch = 0;
 
 	UNUSED(size);
 
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
-		k = '0';
+		pa = '0';
 	if (is_negative)
 		extra_ch = '-';
 	else if (flags & F_PLUS)
@@ -73,7 +73,7 @@ int write_number(int is_negative, int ind, char buffer[],
 		extra_ch = ' ';
 
 	return (write_num(ind, buffer, flags, width, precision,
-		len, k, extra_ch));
+		len, pa, extra_ch));
 }
 
 /**
@@ -84,15 +84,15 @@ int write_number(int is_negative, int ind, char buffer[],
  * @width: width
  * @prec: precision
  * @len: length
- * @k: character
+ * @pa: character
  * @extra_c: extra char
  * Return: characters
  */
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
-	int len, char k, char extra_c)
+	int len, char pa, char extra_c)
 {
-	int j, pa = 1;
+	int j, pa_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0);
@@ -121,11 +121,12 @@ int write_num(int ind, char buffer[],
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], j - 1) + write(1, &buffer[ind], len));
 		}
-		
-		else if (!(flags & F_MINUS) && pa == '0')		{
+
+		else if (!(flags & F_MINUS) && pa == '0')
+		{
 			if (extra_c)
 				buffer[--pa] = extra_c;
-			return (write(1, &buffer[k_start], j - pa) +
+			return (write(1, &buffer[pa_start], j - pa) +
 				write(1, &buffer[ind], len - (1 - pa)));
 		}
 	}
@@ -146,8 +147,7 @@ int write_num(int ind, char buffer[],
  * Return: characters
  */
 int write_unsgnd(int is_negative, int ind,
-	char buffer[],
-	int flags, int width, int precision, int size)
+	char buffer[], int flags, int width, int precision, int size)
 {
 	int len = BUFF_SIZE - ind - 1, j = 0;
 	char pa = ' ';
@@ -177,9 +177,9 @@ int write_unsgnd(int is_negative, int ind,
 
 		buffer[j] = '\0';
 
-		if (flags & F_MINUS)		{
+		if (flags & F_MINUS)
 			return (write(1, &buffer[ind], len) + write(1, &buffer[0], j));
-		}
+
 		else
 		{
 			return (write(1, &buffer[0], j) + write(1, &buffer[ind], len));
@@ -217,7 +217,7 @@ int write_pointer(char buffer[], int ind, int len,
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], len) + write(1, &buffer[3], j - 3));
+			return (write(1, &buffer[ind], len) + write(1, &buffer[3], (j - 3)));
 		}
 		else if (!(flags & F_MINUS) && k == ' ')
 		{
@@ -225,7 +225,7 @@ int write_pointer(char buffer[], int ind, int len,
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[3], j - 3) + write(1, &buffer[ind], len));
+			return (write(1, &buffer[3], (j - 3)) + write(1, &buffer[ind], len));
 		}
 		else if (!(flags & F_MINUS) && k == '0')
 		{
@@ -235,6 +235,7 @@ int write_pointer(char buffer[], int ind, int len,
 			buffer[2] = 'x';
 			return (write(1, &buffer[k_start], j - k_start) +
 				write(1, &buffer[ind], len - (1 - k_start) - 2));
+
 		}
 	}
 	buffer[--ind] = 'x';
